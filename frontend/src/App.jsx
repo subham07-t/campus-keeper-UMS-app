f8b170262c4463888dfacc3c738311e3d51a1352
Subham Saha <saha007subham@gmail.com>
2023-04-16 13:01:51 +0530

    initial commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Login from "./components/Login";
 
 const App = () => {
   return (
     <div>
       <h1>Welcome to Campus Keeper UMS App</h1>
+      <Login />
     </div>
   );
 };
