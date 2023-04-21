5ea46604327e738978b0d263e5eec2f64eb68732
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-04-21 23:48:55 +0530

    BE:auth route and user model set

---
+++ backend/src/routes/authRoute.js
@@ -0,0 +1,8 @@
+const authController = require("../controllers/authController");
+
+const router = require("express").Router();
+
+router.route("/register").post(authController.register);
+router.route("/login").post(authController.login);
+
+module.exports = router;
