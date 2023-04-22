de53cee3b84ce7ab328c05cad5893dfbe6294d8a
Subham Saha <saha007subham@gmail.com>
2023-04-22 17:34:49 +0530

    FE: added css variables

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-
 import React from "react";
 import Login from "./pages/Login";
 import "./App.css";
 
 const App = () => {
-  const[user, setUser] = useState(false);
   return (
     <div>
       <Login />
