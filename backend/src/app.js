6608f08bb92043f908e4f3711bd380569b73751c
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-04-28 23:08:51 +0530

    BE:feat:server landing page added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const cors = require("cors");
 
@@ -13,15 +14,30 @@ connectDB(mongoDbUrl)
   .then(() => console.log("DB connect successfully"))
   .catch((error) => console.log(error));
 
-// routes define
-const authRoute = require("./routes/authRoutes");
-const userRoute = require("./routes/userRoutes");
-
 //middleware functions
 app.use(cors());
 app.use(express.json());
+app.use("/", express.static(path.join(__dirname, "..", "/public")));
+
+// routes define
+const homeRoute = require("./routes/home");
+const authRoute = require("./routes/authRoutes");
+const userRoute = require("./routes/userRoutes");
+app.use("/", homeRoute);
 app.use("/auth", authRoute);
 app.use("/user", userRoute);
+
+// Handle 404 errors
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "..", "/src/views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ message: "404 Not Found" });
+  } else {
+    res.type("txt").send("404 Not Found");
+  }
+});
 
 // server listening
 app.listen(port, () => {
