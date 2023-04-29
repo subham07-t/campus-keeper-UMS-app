5e84d798cb7e1ef4e6f344df3c348e8f8e93711c
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-04-29 13:05:42 +0530

    deploy test 4

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,61 +1,61 @@
-const User = require("../models/User");
-const asyncHandler = require("express-async-handler");
-// const bcrypt = require("bcrypt");
-const { generateFromEmail } = require("unique-username-generator");
-// const CryptoJS = require("crypto-js");
-// const jwt = require("jsonwebtoken");
+// const User = require("../models/User");
+// const asyncHandler = require("express-async-handler");
+// // const bcrypt = require("bcrypt");
+// const { generateFromEmail } = require("unique-username-generator");
+// // const CryptoJS = require("crypto-js");
+// // const jwt = require("jsonwebtoken");
 
-const register = asyncHandler(async (req, res) => {
-  const { email, password, roles } = req.body;
+// const register = asyncHandler(async (req, res) => {
+//   const { email, password, roles } = req.body;
 
-  if (!email || !password || !Array.isArray(roles) || !roles.length) {
-    return res.status(400).json({ message: "All fields are required" });
-  }
+//   if (!email || !password || !Array.isArray(roles) || !roles.length) {
+//     return res.status(400).json({ message: "All fields are required" });
+//   }
 
-  const username = generateFromEmail(email, 3);
+//   const username = generateFromEmail(email, 3);
 
-  const isEmailExist = await User.findOne({ email }).lean().exec();
-  if (isEmailExist) {
-    return res.status(409).json({ message: "Email already exist" });
-  }
+//   const isEmailExist = await User.findOne({ email }).lean().exec();
+//   if (isEmailExist) {
+//     return res.status(409).json({ message: "Email already exist" });
+//   }
 
-  const hashedPwd = await bcrypt.hash(password, 10);
+//   // const hashedPwd = await bcrypt.hash(password, 10);
 
-  const userObject = { username, email, password: hashedPwd, roles };
-  const newUser = await User.create(userObject);
+//   const userObject = { username, email, password: password, roles };
+//   const newUser = await User.create(userObject);
 
-  if (newUser) {
-    //created
-    res.status(201).json({ message: `New user ${email} created` });
-  } else {
-    res.status(400).json({ message: "Invalid user data received" });
-  }
-});
+//   if (newUser) {
+//     //created
+//     res.status(201).json({ message: `New user ${email} created` });
+//   } else {
+//     res.status(400).json({ message: "Invalid user data received" });
+//   }
+// });
 
-const login = asyncHandler(async (req, res) => {
-  const { email, password } = req.body;
+// const login = asyncHandler(async (req, res) => {
+//   const { email, password } = req.body;
 
-  const user = await User.findOne({ email }).lean().exec();
+//   const user = await User.findOne({ email }).lean().exec();
 
-  if (!user) {
-    res.status(401).json("User not found");
-  } else {
-    const isMatch = await bcrypt.compare(password, user.password);
+//   if (!user) {
+//     res.status(401).json("User not found");
+//   } else {
+//     const isMatch = await bcrypt.compare(password, user.password);
 
-    if (isMatch) {
-      const username = user.username;
-      res.status(200).json({ msg: "success", username });
-    } else {
-      res.status(401).json("Wrong Credentials");
-    }
-  }
-});
+//     if (isMatch) {
+//       const username = user.username;
+//       res.status(200).json({ msg: "success", username });
+//     } else {
+//       res.status(401).json("Wrong Credentials");
+//     }
+//   }
+// });
 
-module.exports = {
-  register,
-  login,
-};
+// module.exports = {
+//   register,
+//   login,
+// };
 
-// POST /api/auth/register - Register a new user
-// POST /api/auth/login - Authenticate a user and generate a token
-// POST /api/auth/logout - Log out the current user
+// // POST /api/auth/register - Register a new user
+// // POST /api/auth/login - Authenticate a user and generate a token
+// // POST /api/auth/logout - Log out the current user
