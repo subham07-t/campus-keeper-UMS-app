d6034d269ce62cfaf70a239064df034887336f05
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-04-30 01:15:41 +0530

    FE:server-connected

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -1,7 +1,24 @@
+// import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
 const Profile = () => {
+  // const [first, setfirst] = useState("");
+  const test = async () => {
+    const res = await fetch("https://campus-keeper-server.vercel.app", {
+      method: "GET",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const url = res.url;
+    window.location.replace(url);
+    // <Navigate to={url} />;
+  };
   return (
     <div className="student__main-div">
       <h1>Admin Profile</h1>
+      {/* <p>{first}</p> */}
+      <button onClick={test}>test</button>
     </div>
   );
 };
