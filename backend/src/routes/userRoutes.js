670fc24d54a9dc82ce4f9ba7937261bbf7b469ec
subham07-t <82716446+subham07-t@users.noreply.github.com>
2023-05-01 23:38:58 +0530

    BE:preview server tested

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,11 +1,11 @@
-// const router = require("express").Router();
-// const userController = require("../controllers/userController");
+const router = require("express").Router();
+const userController = require("../controllers/userController");
 
-// router.route("/").get(userController.getAllUsers);
-// router.route("/:role/:id").put(userController.createRoleDetail);
-// router.route("/:id").delete(userController.deleteUser);
-// router
-//   .route("/:role/:id")
-//   .patch(userController.updateRoleDetail)
-//   .delete(userController.deleteRoleDetail);
-// module.exports = router;
+router.route("/").get(userController.getAllUsers);
+router.route("/:role/:id").put(userController.createRoleDetail);
+router.route("/:id").delete(userController.deleteUser);
+router
+  .route("/:role/:id")
+  .patch(userController.updateRoleDetail)
+  .delete(userController.deleteRoleDetail);
+module.exports = router;
